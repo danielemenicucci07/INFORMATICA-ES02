7c0525410e706828334d0049ca9f794e55826512
ciao mondo <113690942+danielemenicucci07@users.noreply.github.com>
2023-10-10 10:02:16 +0200

    Add files via upload

--- tabella_menicucci.cpp
+++ tabella_menicucci.cpp
@@ -20,27 +20,50 @@ struct data//inizializzo record
 
  struct studente//inizializzo record
 {
-	char cog[N];
+	char cognome[N];
 	char nome[N];
 	data nascita;
 	int voti[V];
 } typedef studente;
 
-void caricaTab(studente []);
+void caricaTab(studente []);//dichiaro la funzione carica
 
-void stampaTab(studente []);
+void stampaTab(studente []);//dichiaro la funzione stampa
+
+int ricercCog(studente [],char []);//dichiaro la funzione ricerca cognome
+
+int stampaMedia(studente []);//dichiaro la funzione stampa media
+
+int Studenti10(studente t[]);//dichiaro la funzione studenti10
 
 int main()
 {
 	srand(time(NULL));
 	studente tab[C];
+	char cognome[N];
+	int r;
 	
 	caricaTab(tab);//chiamo la funzione carica nel main
 	
 	stampaTab(tab);//chiamo la funzione stampa nel main 
 	
+	printf("inserisci il cognome che vuoi trovare: ");
+	scanf("%s",cognome);
+	
+	r= ricercCog(tab,cognome);//chiamo la funzione ricerca cognome
+	if(r!=-1)
+	printf("\n\n il cognome e':%s\tl'indice di riga del cognome e':%d",tab[r],r);
+	else
+	printf("cognome non trovato");
+	
+	r=stampaMedia(tab);//chiamo la funzione stampa media voti
+	printf("\n\n%d studenti hanno la media >= a 6",r);
+	
+	r=Studenti10(tab);//chiamo la funzione studenti10
+	printf("\n\n%d studenti hanno almeno un 10 nei voti",r);
+	
 }
-
+//carica tabella con informazioni chieste all’utente e/o generate con random 
 void caricaTab(studente t[]) //inizializzo funzione carica tabella
 {
 	int i=0;
@@ -48,7 +71,7 @@ void caricaTab(studente t[]) //inizializzo funzione carica tabella
 	for(i=0;i<C;i++)
 	{
 		printf("inserisci il tuo cognome: ");
-		scanf("%s",t[i].cog);
+		scanf("%s",t[i].cognome);
 		printf("inserisci il tuo nome: ");
 		scanf("%s",t[i].nome);
 		printf("inserisci il giorno in cui sei nato: ");
@@ -65,7 +88,7 @@ void caricaTab(studente t[]) //inizializzo funzione carica tabella
 	
 	}
 }
-
+//stampa tabella su monitor: si curi quanto viene visualizzato allineando colonne e andando a capo per ogni riga, ecc.
 void stampaTab(studente t[])//inizializzo la funzione stampaTab
 {
 	int i=0;
@@ -73,12 +96,12 @@ void stampaTab(studente t[])//inizializzo la funzione stampaTab
 	printf("COGNOME\t\t");
 	printf("NOME\t\t");
 	printf("DATA\t\t");
-	printf("\tVOTI");
+	printf(" VOTI");
 	printf("\n\n");
 	
 	for(j=0;j<C;j++)
 	{
-		printf("%-15s",t[j].cog);
+		printf("%-15s",t[j].cognome);
 		printf("%-15s",t[j].nome);
 		printf("%3d/",t[j].nascita.gg);
 		printf("%s/",t[j].nascita.mese);
@@ -94,4 +117,53 @@ void stampaTab(studente t[])//inizializzo la funzione stampaTab
 	}
 	
 }
-
+/*ricerca cognome restituendo indice di riga; se trovato, nel main si visualizzi il nome dello studente, 
+altrimenti si visualizzi “cognome non trovato”*/
+int ricercCog(studente t[],char cog[])//inizializzo funzione ricerca cognome
+{
+	int j=0;
+	
+	for(j=0;j<C;j++)
+	{
+		if(strcmp(t[j].cognome,cog)==0)	return j;
+	}
+	return -1;
+}
+/*stampa su monitor il cognome dello studente seguito dalla media dei suoi voti. 
+La funzione restituisce quante medie sono maggiori o uguali a 6 e,
+nel main si effettui printf opportuna (es. x studenti hanno media dei voti >= di 6)*/
+int stampaMedia(studente t[])//inizializzo funzione stampa media
+{
+	int i=0;
+	float m=0;
+	int c=0;
+	int j=0;
+	
+	for(i=0;i<C;i++,m=0)
+	{
+		for(j=0;j<V;j++)
+		m=m+t[i].voti[j];
+		m=m/V;
+		if(m>=6) c++;
+		printf("\n\nla media voti dello studente %s e': %.2f", t[i].cognome,m);
+	}
+	return c;
+}
+/*nella funzione si contano quanti studente hanno almeno un voto = 10. La funzione restituisce il valore. N. B. : non si chiede quanti 
+10 ma quanti studenti, cioè lo studente che ha più di un 10, va contato solo una volta*/
+int Studenti10(studente t[])
+{
+	int i=0;
+	int j=0;
+	int c=0;
+	
+	for(i=0;i<C;i++)
+	{
+		for(j=0;j<V;j++)
+		if(t[i].voti[j]==10)
+		c++;
+		j=V;
+	}
+	
+	return c;
+}
