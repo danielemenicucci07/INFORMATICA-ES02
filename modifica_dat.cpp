da751b5b1f4351e1e131942ff92f48bcb73f8026
ciao mondo <113690942+danielemenicucci07@users.noreply.github.com>
2023-11-27 08:50:14 +0100

    Update modifica_dat.cpp

--- modifica_dat.cpp
+++ modifica_dat.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<stdiopo.h>
 #include<time.h>
 #include<stdlib.h>
 #include<string.h>
@@ -24,7 +24,7 @@ struct alunno {
 void crea(char[]);
 
 void modifica( char[], char[], char[]);//Si sostituisca su file il cognome1 (per tutte le occorrenze presenti nel file) con cognome2.
-//Si restituisca quante volte è stato incontrato cognome1
+//Si restituisca quante volte Ã¨ stato incontrato cognome1
 
 
 int main()
@@ -39,7 +39,7 @@ void crea(char file[])
     alunno buffer;
     FILE * puntfile = fopen(file, "ab");// ab perche sono file binari
 
-    if(puntfile!=NULL)// controllo se c'è un errore nell'apertura
+    if(puntfile!=NULL)// controllo se c'Ã¨ un errore nell'apertura
 	{
         for(int i=0;i<T;i++)
         {
@@ -68,14 +68,14 @@ void modifica(char file[],char cog1[],char cog2[])
 	
 	alunno buffer;
 	
-	if(puntfile!=NULL)//se il file si è aperto
+	if(puntfile!=NULL)//se il file si Ã¨ aperto
 	
 
-	while(!feof(puntfile))//finchè il file non è finito
+	while(!feof(puntfile))//finchÃ¨ il file non Ã¨ finito
 	{
-		fread(&buffer,sizeof(alunno),1,puntfile);//leggo un record alla volta che ci è scritto sul file
+		fread(&buffer,sizeof(alunno),1,puntfile);//leggo un record alla volta che ci Ã¨ scritto sul file
 		
-		if(strcmp(buffer.cognome,cog1)==0)//se il cognome è uguale a quello che cerco
+		if(strcmp(buffer.cognome,cog1)==0)//se il cognome Ã¨ uguale a quello che cerco
 		{
 			strcpy(buffer.cognome,cog2);//sostituisco il cognome
 			fseek(puntfile,-sizeof(alunno),SEEK_CUR);//mi riposiziono sulla posizione corretta tornando indietro di una posizione
