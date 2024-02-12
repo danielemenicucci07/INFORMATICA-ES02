9d66bc95fc6de2b5e1b7929c0cd53ab76f57e705
ciao mondo <danielemenicucci3@gmail.com>
2024-02-12 08:47:00 +0000

    commit

--- JAVASCRIPT/JAVASCRIPT/ES.C/persona.js
+++ JAVASCRIPT/JAVASCRIPT/ES.C/persona.js
@@ -1,9 +1,9 @@
 let persona = {}// dichiaro la persona vuota perche le chiediamo all'utente
 
-persona.nome = prompt("Inserisci il nome")
+persona.nome = prompt("Inserisci il nome")//chiedo il nome all'utente
 
-persona.cognome = prompt("Inserisci il cognome")
+persona.cognome = prompt("Inserisci il cognome")//chiedo il cognome all'utente
 
-persona.eta = prompt("Inserisci l'eta")
+persona.eta = prompt("Inserisci l'eta")//chiedo l'eta all'utente
 
 console.log(persona)// stampa a schermo le caratteristiche delle persone
--- JAVASCRIPT/JAVASCRIPT/scatola/scatola.js
+++ JAVASCRIPT/JAVASCRIPT/scatola/scatola.js
@@ -1,9 +1,9 @@
-let larghezza = prompt("inserisci larghezza scatola")// LET = non ci sono tipi su javascript e LET dichiara una variabile
+let larghezza = prompt("inserisci larghezza scatola")// LET = non ci sono tipi su javascript e LET dichiara una variabile,chiedo larghezza all'utente
 
-let altezza = prompt("inserisci altezza scatola")
+let altezza = prompt("inserisci altezza scatola")//chiedo altezzaa all'utente
 
-let lunghezza = prompt("inserisci lunghezza scatola")
+let lunghezza = prompt("inserisci lunghezza scatola")//chiedo lunghezza all'utente
 
-let volume = larghezza * altezza * lunghezza
+let volume = larghezza * altezza * lunghezza//formula per il volume
 
 console.log("volume scatola = " + volume)// stampa a schermoil volume della scatola
