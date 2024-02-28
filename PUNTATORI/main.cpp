7e4809adb65ce8a62c0b44368e23959392075928
ciao mondo <danielemenicucci3@gmail.com>
2024-02-28 12:38:32 +0000

    commit

---
+++ PUNTATORI/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "pila.h"
+using namespace std;
+
+int main() 
+{
+    Pila p(100);
+
+    p.push(10);
+    p.push(9);
+    p.push(2);
+    p.pop();
+    p.push(45);
+    p.push(55);
+
+    p.stampa();
+}
