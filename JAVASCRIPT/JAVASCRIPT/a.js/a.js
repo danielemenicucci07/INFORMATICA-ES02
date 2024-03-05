fba01e40cf5861100676210ebffe50e72cefe39f
ciao mondo <danielemenicucci3@gmail.com>
2024-03-05 08:58:28 +0000

    commit

--- JAVASCRIPT/JAVASCRIPT/a.js/a.js
+++ JAVASCRIPT/JAVASCRIPT/a.js/a.js
@@ -1,5 +1,5 @@
-for(let i=100;i>=0;i-=20)
+for(let i=100;i>=0;i-=20)// ciclo for che parte da 100 e decrementa di 20 fino a quando non arriva a 0
 {
-    console.log(i);
+    console.log(i);// stampa il contatore quindi 100 80 60 40 20 0
 
 }
--- JAVASCRIPT/JAVASCRIPT/b..js/b.js
+++ JAVASCRIPT/JAVASCRIPT/b..js/b.js
@@ -1,10 +1,10 @@
 libro={
-    titolo:"harry potter",
-    autore:"J. K. Rowling",
-    data:"25/02/2024",
-    genere:"fantasy",
-    pagine:"304"
+    titolo:"harry potter", // titolo 
+    autore:"J. K. Rowling", //autore
+    data:"25/02/2024", //data 
+    genere:"fantasy", //genere
+    pagine:"304" //pagine
     }
     
-    for(i in libro)
-        console.log(`${i}= ${libro[i]}`)
+    for(i in libro) // i è la chiave 
+        console.log(`${i}= ${libro[i]}`) // stampa ogni chiave di una alla volta quindi titolo, autore, ecc..
