b6c614d9c48067ff074fe46c1371e5c1b65f0dab
ciao mondo <danielemenicucci3@gmail.com>
2024-03-05 08:51:35 +0000

    commit

---
+++ JAVASCRIPT/JAVASCRIPT/b..js/b.js
@@ -0,0 +1,10 @@
+libro={
+    titolo:"harry potter",
+    autore:"J. K. Rowling",
+    data:"25/02/2024",
+    genere:"fantasy",
+    pagine:"304"
+    }
+    
+    for(i in libro)
+        console.log(`${i}= ${libro[i]}`)
