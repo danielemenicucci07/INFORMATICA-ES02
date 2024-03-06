33467084f075c152524bfcdb3925bdc6a10fc6d8
ciao mondo <danielemenicucci3@gmail.com>
2024-03-06 11:48:00 +0000

    commit

--- JAVASCRIPT/JAVASCRIPT/c.js/c.js
+++ JAVASCRIPT/JAVASCRIPT/c.js/c.js
@@ -1,28 +1,28 @@
-n1 = parseInt(prompt("Inserisci il primo numero: "))// stampa la frase
-n2 = parseInt(prompt("inserisci il secondo numero: "))// stampa la frase
-segno = prompt("iserisci uno di questi segni per definire che operazione fare + - / * ** ")// stampa la frase
+num1 = parseInt(prompt("Inserisci il primo numero: "))// stampa la frase
+num2 = parseInt(prompt("inserisci il secondo numero: "))// stampa la frase
+simbolo = prompt("iserisci uno di questi simboli matematici per definire che operazione fare + - / * ** ")// stampa la frase
 
-if(Number.isNaN(n1) || Number.isNaN(n2)){//  permette di verificare se una variabile contiene un numero o una stringa convertibile in un numero
+if(Number.isNaN(num1) || Number.isNaN(num2)){//  permette di verificare se una variabile contiene un numero o una stringa convertibile in un numero
     alert("uno dei due numeri non e' un numero")// se l'if è vero quindi se trova una stringa e non un numero stampa l'alert
 }
 else{
-    switch(segno){
-        case "+": risultato = n1 + n2// se è stato inserito il + si fa una somma quindi n1 + n2
+    switch(simbolo){
+        case "+": risultato = num1 + num2// se è stato inserito il + si fa una somma quindi n1 + n2
                   break
 
-        case "-": risultato = n1 - n2// se è stato inserito il - si fa una sottrazione quindi n1 - n2
+        case "-": risultato = num1 - num2// se è stato inserito il - si fa una sottrazione quindi n1 - n2
                   break
 
-        case "/": risultato = n1 / n2// se è stato inserito lo / si fa una divisione quindi n1 / n2
+        case "/": risultato = num1 / num2// se è stato inserito lo / si fa una divisione quindi n1 / n2
                   break
         
-        case "*": risultato = n1 * n2// se è stato inserito l'* si fa una moltiplicazione quindi n1 * n2
+        case "*": risultato = num1 * num2// se è stato inserito l'* si fa una moltiplicazione quindi n1 * n2
                   break
 
-        case "**": risultato = n1 ** n2// se è stato inserito il ** si fa una potenza quindi n1 ** n2
+        case "**": risultato = num1 ** num2// se è stato inserito il ** si fa una potenza quindi n1 ** n2
                   break
 
-        default: alert("segno inserito inesistente")// se il simbolo che hai inserito non c'è tra quelli possibili ti stampa l'alert
+        default: alert("simbolo inserito inesistente")// se il simbolo che hai inserito non c'è tra quelli possibili ti stampa l'alert
         
     }
     
