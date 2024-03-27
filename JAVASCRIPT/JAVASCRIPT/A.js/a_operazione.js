9dec679e356fe1c9d11722c37349ad295346e3d1
ciao mondo <danielemenicucci3@gmail.com>
2024-03-27 12:04:30 +0000

    commit

--- JAVASCRIPT/JAVASCRIPT/A.js/a_operazione.js
+++ JAVASCRIPT/JAVASCRIPT/A.js/a_operazione.js
@@ -1,15 +1,15 @@
-num1 = parseInt(prompt("Inserisci il primo numero: "))// stampa la frase
-num2 = parseInt(prompt("inserisci il secondo numero: "))// stampa la frase
+n1 = parseInt(prompt("Inserisci il primo numero: "))// stampa la frase
+n2 = parseInt(prompt("inserisci il secondo numero: "))// stampa la frase
 simbolo = prompt("iserisci uno di questi simboli matematici per definire che operazione fare + - / * ** ")// stampa la frase
 
-if(Number.isNaN(num1) || Number.isNaN(num2)){//  permette di verificare se una variabile contiene un numero o una stringa convertibile in un numero
+if(Number.isNaN(n1) || Number.isNaN(n2)){//  permette di verificare se una variabile contiene un numero o una stringa convertibile in un numero
     alert("uno dei due numeri non e' un numero")// se l'if è vero quindi se trova una stringa e non un numero stampa l'alert
 }
 else{
     switch(simbolo){
 
         case "+": 
-            const somma = function(a,b) {return a + b}
+            const somma = function(a,b) {return a + b};
             alert("il risultato è: " + somma(n1,n2));
             break
 
